2b2202edfd51ef865dbfc2f6bb86a21e1e8d76ed
Willian Alves <wba.alves@gmail.com>
2019-09-28 22:17:21 -0300

    create header

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({ })
+export default class Header extends Vue { }
+</script>
+
+<style scoped>
+  .header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header a {
+    color: #fff;
+    padding-right: 5px;
+  }
+</style>
