fb90accc4e8e1a44ea9bc85f858b6db06d519c75
Willian Alves <wba.alves@gmail.com>
2019-09-28 21:47:13 -0300

    create todo component and definition

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+  import TodoItem from "./TodoItem.vue";
+  import { Todo } from "../types/Todo";
+
+  @Component({
+    components: {
+      TodoItem
+    },
+  })
+  export default class Todos extends Vue {
+    @Prop() private todos!: Todo[];
+  }
+</script>
+
+<style scoped>
+
+</style>
