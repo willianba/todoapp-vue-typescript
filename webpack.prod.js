ef4f242dafe035179238b9b455bb8110eadf31cf
Willian Alves <wba.alves@gmail.com>
2019-09-28 23:31:31 -0300

    modify prod webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,18 +4,7 @@ const webpack = require("webpack");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: "#source-map",
   optimization: {
     minimize: true
-  },
-  plugins: (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: "production"
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+  }
 });
